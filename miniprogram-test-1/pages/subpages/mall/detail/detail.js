365d511cc1b8c0dbf3f9cf96b6b83f3f9d7d9e9d
Thea12138 <1045424353@qq.com>
2019-03-23 13:15:42 +0800

    0323

--- miniprogram-test-1/pages/subpages/mall/detail/detail.js
+++ miniprogram-test-1/pages/subpages/mall/detail/detail.js
@@ -9,19 +9,23 @@ Page({
     proInstructions: '',
     proDetails: '',
     proImage:'',
-    thumimage:''
+    thumimage:'',
+    product_id: ''
   },
   onLoad: function (options) {
     var self = this;
     var name = wx.getStorageSync('name');
+    var product_id = wx.getStorageSync('product_id');
     var price = wx.getStorageSync('price');
     var Instructions = wx.getStorageSync('Instructions');
     var details = wx.getStorageSync('details');
     var image = wx.getStorageSync('image');
     var thumimage = wx.getStorageSync('thumimage');
+    // console.log(product_id);
     wx.setStorageSync('thumimage', thumimage);
     self.setData({
       proName: name,
+      product_id: product_id,
       proPrice: price,
       proInstructions: Instructions,
       proDetails: details,
@@ -30,9 +34,38 @@ Page({
     
   },
   buybutton: function () {
-    wx.navigateTo({
-      url: '../buy/buy',
+    wx.showToast({
+      title: '成功加入购物车',
+      icon: 'succes',
+      duration: 10000
     })
+
+    setTimeout(function () {
+      wx.hideToast()
+    }, 1000)
+
+    var product_id = this.data.product_id;
+    wx.request({
+      url: serverURL + '/shoppingCar/insertCarinfo',
+      data: {
+        token: app.globalData.token,
+        uid: app.globalData.user_id,
+        pid: product_id
+      },
+      success: function (res) {
+
+      }, fail: function (res) {
+        // wx.showToast({
+        //   title: '加入购物车失败',
+        //   icon: 'succes',
+        //   duration: 10000
+        // })
+
+        // setTimeout(function () {
+        //   wx.hideToast()
+        // }, 1000)
+      }
+    }) 
   },
   onShow: function () {
     
