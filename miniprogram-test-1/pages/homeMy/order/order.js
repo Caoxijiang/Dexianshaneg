9362c7b939be2d1b90ae71a48b325a3b9bf247d2
Thea12138 <1045424353@qq.com>
2019-03-26 10:33:54 +0800

    0326

--- miniprogram-test-1/pages/homeMy/order/order.js
+++ miniprogram-test-1/pages/homeMy/order/order.js
@@ -9,6 +9,7 @@ Page({
     totalPrice: 0,           // 总价，初始为0
     selectAllStatus: true    // 全选状态，默认全选
   },
+  
   onShow() {
     var self = this;
     console.log("sdgsdf");
@@ -19,11 +20,16 @@ Page({
         uid: app.globalData.user_id,
       },
       success: function (res) {
-        self.setData({
-          hasList: true,
-          carts: res.data
-        })     
-        console.log(res.data);
+        if (res.data[0].product_id == null){
+            //console.log("kong");
+        }else{
+          self.setData({
+            hasList: true,
+            carts: res.data
+          }) 
+        }
+            
+        
       }, fail: function (res) { }
     })
 
