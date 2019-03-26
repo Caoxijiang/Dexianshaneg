2972eec4b786be340b4ddde414bfb3cd3f54aa93
Thea12138 <1045424353@qq.com>
2019-03-26 09:44:10 +0800

    0326

--- miniprogram-test-1/host/host.js
+++ miniprogram-test-1/host/host.js
@@ -1,7 +1,7 @@
 module.exports = {
 
      // SERVER_URL: "https://www.iv2018.cn"
-    SERVER_URL: "http://localhost:3006"
-     // SERVER_URL: "http://192.168.3.117:3006"
+    //SERVER_URL: "http://localhost:3006"
+    SERVER_URL: "http://192.168.3.117:3006"
 
 }
