62e625013490b4393bcadf6580b3d2682bfc3438
Caoxijiang <cao_xijiang@163.com>
2019-03-26 11:33:50 +0800

    '0326'

--- server/zdh/dao/ordersSqlMapping.js
+++ server/zdh/dao/ordersSqlMapping.js
@@ -1,8 +1,8 @@
 
 var orders={
     insertorderInfo:"insert into orders_info(user_id,order_Num,order_total,order_tname,order_phone,order_status,order_starttime,order_endtime,order_mark,order_remarks) values(?,?,?,?,?,0,?,0,?,?)",
-    selectAllorderInfo:"select o.*,t.pid,t.num,p.* from orders_info as o LEFT JOIN order_prod as t on o.order_id=t.oid LEFT JOIN product_info as p on t.pid=p.product_id",
-    selectorderByphoneNum:"select o.*,t.pid,t.num,p.* from orders_info as o LEFT JOIN order_prod as t on o.order_id=t.oid LEFT JOIN product_info as p on t.pid=p.product_id",
+    selectAllorderInfo:"select o.*,t.pid,t.num,p.*,s.*  from orders_info as o LEFT JOIN order_prod as t on o.order_id=t.oid LEFT JOIN product_info as p on t.pid=p.product_id LEFT JOIN goods_address s on o.user_id=s.user_id where sign=1",
+    selectorderByphoneNum:"select o.*,t.pid,t.num,p.*from orders_info as o LEFT JOIN order_prod as t on o.order_id=t.oid LEFT JOIN product_info as p on t.pid=p.product_id",
     updateorderstatus:"update  orders_info set order_status=?,order_endtime=? where order_Num=?",
 
     wxselectlistbyphone:"select o.*,t.pid,t.num,p.* from orders_info as o LEFT JOIN order_prod as t on o.order_id=t.oid LEFT JOIN product_info as p on t.pid=p.product_id  where o.user_id=?",
